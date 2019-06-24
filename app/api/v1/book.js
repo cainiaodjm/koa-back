7a8183da351aad7ee3c3da15073f7fd3056d697e
cainiaodjm <cainiaodjm@163.com>
2019-06-25 01:00:45 +0800

    update

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,75 +1,88 @@
-const  bcrypt=require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const Router = require('koa-router')
 
-const {RegisterValidator,UserValidator,
-  BookValidator,PositiveIntegerValidator,
+const { RegisterValidator, UserValidator,
+  BookValidator, PositiveIntegerValidator,
   SearchValidator,
   CommentValidator
-}=require('../../validators/validator')
-const {Book}= require('../../models/book')
-const {HotBook}=require('../../models/hot_book')
-const {Flow}=require('../../models/flow')
-const {BookComment}=require('../../models/book_comment')
-const {Success,NotFound}=require('../../../core/http-exception')
-const {Auth}=require('../../../middlewares/auth')
+} = require('../../validators/validator')
+const { Book } = require('../../models/book')
+const { HotBook } = require('../../models/hot_book')
+const { Flow } = require('../../models/flow')
+const { BookComment } = require('../../models/book_comment')
+const { Success, NotFound } = require('../../../core/http-exception')
+const { Auth } = require('../../../middlewares/auth')
 const router = new Router({
-  prefix:'/v1/book'
+  prefix: '/v1/book'
 })
 
-router.get('/:id/detail',async (ctx,next)=>{
-  const v= await new PositiveIntegerValidator().validate(ctx)
-  let id=v.get('path.id')
-  let book=await new Book(id).detail()
-  throw new Success("查询成功",0,{
-    data:book
+router.get('/:id/detail', async (ctx, next) => {
+  const v = await new PositiveIntegerValidator().validate(ctx)
+  let id = v.get('path.id')
+  let book = await new Book(id).detail()
+  throw new Success("查询成功", 0, {
+    data: book
   })
 })
-router.get('/search',async (ctx,next)=>{
-  const v=await new SearchValidator().validate(ctx)
-  const res= await Book.search(v.get('query.q'),v.get('query.start'),v.get('query.count'))
-  throw new Success("查询成功",0,{
-    data:res
+router.get('/search', async (ctx, next) => {
+  const v = await new SearchValidator().validate(ctx)
+  const res = await Book.search(v.get('query.q'), v.get('query.start'), v.get('query.count'))
+  throw new Success("查询成功", 0, {
+    data: res
   })
 })
-router.get('/:book_id/favor',new Auth().m,async(ctx,next)=>{
-  const v= await new PositiveIntegerValidator().validate(ctx,{
-    id:"book_id"
+router.get('/hot_keyword', async (ctx, next) => {
+  let hoeKeyword = {
+    "hot": [
+      "Fluent Python",
+      "Python",
+      "小程序Java核心编程",
+      "慕课网7七月",
+      "微信小程序开发入门与实践",
+      "C++"
+    ]
+  }
+  throw new Success('查询成功',0,hoeKeyword)
+})
+router.get('/:book_id/favor', new Auth().m, async (ctx, next) => {
+  const v = await new PositiveIntegerValidator().validate(ctx, {
+    id: "book_id"
   })
-  const uid=ctx.auth.uid
-  const res=await Book.getBookFavor(uid,v.get('path.book_id'))
-  throw new Success('query success',0,{
-    data:res
+  const uid = ctx.auth.uid
+  const res = await Book.getBookFavor(uid, v.get('path.book_id'))
+  throw new Success('query success', 0, {
+    data: res
   })
 })
-router.get('/favor/count',new Auth().m, async(ctx,next)=>{
-  const uid=ctx.auth.uid
-  const count=await Book.getMyFavorBookCount(uid)
-  throw new Success("查询成功",0,{
-    data:count
+router.get('/favor/count', new Auth().m, async (ctx, next) => {
+  const uid = ctx.auth.uid
+  const count = await Book.getMyFavorBookCount(uid)
+  throw new Success("查询成功", 0, {
+    data: count
   })
 })
-router.get('/hot_list',async (ctx,next)=>{
-  const books =await HotBook.getAll()
-  throw new Success('查询成功',0,{
-    data:books
+router.get('/hot_list', async (ctx, next) => {
+  const books = await HotBook.getAll()
+  throw new Success('查询成功', 0, {
+    data: books
   })
 })
 
 //添加书籍
-router.post('/',async (ctx,next)=>{ 
-  const v=await new BookValidator().validate(ctx)
-  let title=v.get('body.title')
-  let content=v.get('body.content')
-  let author=v.get('body.author')
-  let nationality=v.get('body.nationality')
-  let press_name=v.get('body.press_name')
-  let press_date=v.get('body.press_date')
-  let page_count=v.get('body.page_count')
-  let price=v.get('body.price')
-  
-  
-  let paperback=v.get('body.paperback')
-  const book={
+router.post('/', async (ctx, next) => {
+  const v = await new BookValidator().validate(ctx)
+  let title = v.get('body.title')
+  let content = v.get('body.content')
+  let author = v.get('body.author')
+  let nationality = v.get('body.nationality')
+  let press_name = v.get('body.press_name')
+  let press_date = v.get('body.press_date')
+  let page_count = v.get('body.page_count')
+  let price = v.get('body.price')
+
+
+  let paperback = v.get('body.paperback')
+  const book = {
     title,
     content,
     author,
@@ -80,35 +93,35 @@ router.post('/',async (ctx,next)=>{
     price,
     paperback
   }
-  if(v.get('body.image')){
-    book.image=v.get('body.image')
+  if (v.get('body.image')) {
+    book.image = v.get('body.image')
   }
 
-  const _book=await Book.create(book)
-  const flow={
-    "type_id":_book.id,
-    "type":_book.type,
+  const _book = await Book.create(book)
+  const flow = {
+    "type_id": _book.id,
+    "type": _book.type,
   }
   //创建点赞记录 初始点赞记录为0
   await Flow.create(flow)
-  throw new Success('','',{
-    data:_book
+  throw new Success('', '', {
+    data: _book
   })
-  
+
 })
 //获取热门书籍(概要)
-router.get('/hot_list',async(ctx,next)=>{
-  let books=await Book.findAll({
+router.get('/hot_list', async (ctx, next) => {
+  let books = await Book.findAll({
   })
-  ctx.body=books
+  ctx.body = books
 })
 //增加短评
-router.post('/add/short_comment',new Auth().m,async (ctx,next)=>{
-  const v=await new CommentValidator().validate(ctx)
-  const res= await BookComment.addComment(v.get('body.book_id'),v.get('body.content'))
-  throw new Success('添加短评成功',0,{
-    data:res
+router.post('/add/short_comment', new Auth().m, async (ctx, next) => {
+  const v = await new CommentValidator().validate(ctx)
+  const res = await BookComment.addComment(v.get('body.book_id'), v.get('body.content'))
+  throw new Success('添加短评成功', 0, {
+    data: res
   })
 })
 
-module.exports=router
+module.exports = router
--- test.js
+++ test.js
@@ -1,80 +1,27 @@
-
-
-// function func1(){
-//   func2()
-// }
-// function func2(){
-//   func3()
-// }
-// function func3(){
-  
-// }
-// var one ={x:1}
-// var two =new Object()
-// one.__proto__ ===Object.prototype
-/**
- * 
- */
-// function Person(){
-//   this.name="mark"
-// }
-// var p1= new Person()
-// p1.name="jack"
-/**
- * 声明了Person后 内存中会创建一个对象B
- * Person会有个默认函数 prototype
- * 对象B就是Person的原型对象 简称原型
- * 原型中会有一个constructor属性指向Person
- * 原型对象默认只有属性:constructor  其他都是从Object继承而来的
- */
-// console.log(p1.name)
-// function Person2(){
-// }
-/**  本来Person2的原型对象是可以通过
- *   Person2.prototype.constructor获得
- * 
-*/
-//现在直接 让Person2的原型对象指向新对象
-// Person2.prototype={
-//   name:'kkk'
-// }
-// //
-// var p2=new Person2()
-// console.log(p2.name)
-// var name='aaa'
-// var age=18
-// function test(){
-//   console.log(this)
-//   console.log(this.name)
-// }
-// test()
-
-async function fun1(){
- try {
-   await fun2()
- } catch (error) {
-   console.error(error);
-   
- }
-}
-async function  fun2(){
+const OSS =require('ali-oss')
+const client= new OSS({
+  accessKeyId:"LTAI4Hor5P1itsLt",
+  accessKeySecret:"EKzYbRoqnJzkAcD5Wg9QPAuSRpMK9h",
+  bucket:"cainiaodjm"
+})
+async function listBuckets() {
   try {
-    await fun3()
+    let res=await client.listBuckets()
+    console.log(res)
   } catch (error) {
     console.log(error)
   }
- 
 }
-function fun3(){
-  return new Promise((resolve,reject)=>{
-    setTimeout(() => {
-      const r= Math.random()
-      if(r<0.5){
-        reject('error')
-      }
-    }, 1000); 
-  })
+// listBuckets()
+client.useBucket('cainiaodjm')
+async function list (){
+  try {
+    let result=await client.list({
+      // 'max-keys': 5
+    })
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
 }
-//全局异常处理
-
-fun1()
+list()
